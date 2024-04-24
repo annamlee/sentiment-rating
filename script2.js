35a5f69171427e68d37c78c1bf0318fdc00203da
JacksonYangUCSD <122575102+JacksonYangUCSD@users.noreply.github.com>
2024-04-24 16:01:42 -0700

    Updated script2.js with localstorage

--- script2.js
+++ script2.js
@@ -1,12 +1,22 @@
-// Initialize emotions object
-let emotions = {
-  upset: 0,
-  unhappy: 0,
-  neutral: 0,
-  happy: 0,
-  joyful: 0,
-  total: 0
-};
+//Function to load emotions from local storage (also to initialize emotions)
+function loadEmotions() {
+  const storedEmotion = localStorage.getItem("emotions");
+  if (storedEmotion != undefined){
+    return JSON.parse(storedEmotion);
+  } else {
+    return {
+      upset: 0,
+      unhappy: 0,
+      neutral: 0,
+      happy: 0,
+      joyful: 0,
+      total: 0  
+    }
+  }
+}
+
+// Initializes the emotion object
+let emotions = loadEmotions();
 
 // Function to update the percentages in the HTML
 function updatePercentages(emotions, total) {
@@ -29,6 +39,9 @@ form.addEventListener("submit", (e) => {
 
   emotions[selectedEmotion]++;
   emotions["total"]++;
+
+  //Saves the number to the localStorage
+  localStorage.setItem("emotions", JSON.stringify(emotions));
 
   // Update the percentages in the HTML
   const total = emotions["total"];
